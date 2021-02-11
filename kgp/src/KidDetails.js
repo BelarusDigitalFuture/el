166cc36b97ddefdbdc9be347c017203ade4e7e09
Andrew Osipenko <dev7000@gmail.com>
2021-02-11 09:19:46 +0300

    Use bootstrap on KidDetails wizard step

--- kgp/src/KidDetails.js
+++ kgp/src/KidDetails.js
@@ -1,5 +1,8 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {Col, Container, Form, FormControl, FormGroup, Row} from "react-bootstrap";
+import FormFileInput from "react-bootstrap/FormFileInput";
+import Button from "react-bootstrap/Button";
 
 class KidDetails extends Component {
     constructor() {
@@ -10,30 +13,61 @@ class KidDetails extends Component {
     }
 
     render() {
-        return <form>
-            <h2>Данные о ребенке</h2>
-            <label>
-                Фамилия
-            </label><br />
-            <input type="text"/><br />
-            <label>
-                Имя
-            </label><br />
-            <input type="text"/><br />
-            <label>
-                Отчество
-            </label><br />
-            <input type="text"/><br />
-            <label>
-                Свидельство о рождении
-            </label><br />
-            <input type="file"/><br />
-            <label>
-                Ваш email
-            </label><br />
-            <input type="text"/><br />
-            <Link to="/kindergartens">Дальше</Link>
-        </form>;
+        return <Container><Form>
+            <h1 className='mb-5'>Заявка</h1>
+            <h2 className='mb-5'>Данные о ребенке</h2>
+            <div className='form-row'>
+                <FormGroup className='col-md-6'>
+                    <label>
+                        Имя
+                    </label>
+                    <FormControl name='name'/>
+                </FormGroup>
+                <FormGroup className='col-md-6'>
+                    <label>
+                        Фамилия
+                    </label>
+                    <FormControl/>
+                </FormGroup>
+            </div>
+            <div className='form-row'>
+                <FormGroup className='col-md-6'>
+                    <label>
+                        Отчество
+                    </label>
+                    <FormControl/>
+                </FormGroup>
+            </div>
+            <div className='form-row'>
+                <FormGroup className='col-md-6'>
+                    <label>
+                        Дата рождения
+                    </label>
+                    <FormControl type='date'/>
+                </FormGroup>
+                <FormGroup className='col-md-6'>
+                    <label>
+                        Свидетельство о рождении
+                    </label>
+                    <FormFileInput />
+                </FormGroup>
+            </div>
+            <div className='form-row mb-5'>
+                <FormGroup className='col-md-6'>
+                    <label>
+                        Email
+                    </label>
+                    <FormControl type='email'/>
+                </FormGroup>
+            </div>
+            <Row>
+                <Col md={6}>
+                    <Link to="/kindergartens">
+                        <Button className='btn-block'>Дальше</Button>
+                    </Link>
+                </Col>
+            </Row>
+        </Form></Container>;
     }
 }
 
